51b45151e02900454a7eea35831216946b56e0ca
Js1ne <so2js1ne@gmail.com>
2025-02-15 16:16:01 +1000

    slovekinz-bot

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "7925879815:AAGfIVKaThIVDOs8WEGG5Wr15LGliA4KSPo"  # токен ботика
+CHANNEL_URL = "https://t.me/+7PJMZolAS6VmNWYy"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1002380709443"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1002351257379"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "https://t.me/predat_1w"
+ADMIN_ID = 7944542891 # Ваш айди без скобочек
+PROMO = "PRDT500" # промокод
